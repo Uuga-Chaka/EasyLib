c87039471cf21b245ac5886022c7ef3157a8582b
unknown <Uuga-Chaka>
2018-05-17 00:44:50 -0500

    Initial commit

---
+++ app/src/main/java/icesi/dmi/easylib/HomePage.java
@@ -0,0 +1,13 @@
+package icesi.dmi.easylib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
